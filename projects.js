c2513c76fecc46ca1ef8b9d2b56d1e98440a30c9
OYB <147553938+OYB0@users.noreply.github.com>
2026-04-29 07:00:18 +0300

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,23 +1,23 @@
-// projects.js - القائمة اليدوية لمشاريع وسام (OYB DEVELOPER)
-// لإضافة مشروع جديد، فقط انسخ أحد الكتل { } وألصقها في الأسفل وغير البيانات.
-
 const myProjectsData = [
     {
         name: "Key System",
+        actionType: "github",
         url: "https://github.com/OYB0/OYB/tree/main/Roblox/Executor/KeySystem",
         imageUrl: "https://raw.githubusercontent.com/OYB0/OYB/main/Roblox/Executor/KeySystem/cover.png",
-        mainCategory: "Roblox",      // سيظهر تحت زر Roblox
-        badge: "Executor Script"     // النص الذي سيظهر فوق غلاف المشروع
+        mainCategory: "Roblox",
+        badge: "Executor Script"
     },
     {
         name: "Events Panel",
+        actionType: "github", // التوجيه لرابط جيت هب
         url: "https://github.com/OYB0/OYB/tree/main/Roblox/RobloxStudio/EventsPanel",
         imageUrl: "https://raw.githubusercontent.com/OYB0/OYB/main/Roblox/RobloxStudio/EventsPanel/cover.png",
-        mainCategory: "Roblox",      // سيظهر تحت زر Roblox
-        badge: "Roblox Studio"       // النص الذي سيظهر فوق غلاف المشروع
+        mainCategory: "Roblox",
+        badge: "Roblox Studio"
     },
     {
         name: "Roblox Telegram Monitor",
+        actionType: "github", // التوجيه لرابط جيت هب
         url: "https://github.com/OYB0/OYB/tree/main/Roblox/RobloxStudio/RobloxTelegramMonitor",
         imageUrl: "https://raw.githubusercontent.com/OYB0/OYB/main/Roblox/RobloxStudio/RobloxTelegramMonitor/cover.png",
         mainCategory: "Roblox",
